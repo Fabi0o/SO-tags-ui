9ca0f164e25097a1dad6ac588b28cf2b2832a8d1
Fabi0o <marfab32@gmail.com>
2024-03-27 13:34:35 +0100

    add table with pagination and sorting

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,15 +1,50 @@
 import { Tag } from "../../types/Response";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 type Props = {
   tags: Tag[] | undefined;
 };
 
+const columns: GridColDef[] = [
+  {
+    field: "name",
+    headerName: "Name",
+    flex: 1,
+    align: "center",
+    headerAlign: "center",
+  },
+  {
+    field: "count",
+    headerName: "Count",
+    flex: 1,
+    align: "center",
+    headerAlign: "center",
+  },
+];
+
 function Table({ tags }: Props) {
+  const rows = tags!.map(({ name, count }, id) => {
+    return {
+      id,
+      name,
+      count,
+    };
+  });
+
   return (
     <>
-      {tags!.map((tag) => (
-        <div key={tag.name}>{tag.name}</div>
-      ))}
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        autosizeOptions={{
+          expand: true,
+        }}
+      />
     </>
   );
 }
