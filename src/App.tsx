2f08e1cc8156afc11eaf3edd507dff18667aed8e
Fabi0o <marfab32@gmail.com>
2024-03-27 13:38:57 +0100

    add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import Home from "./Home";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const queryClient = new QueryClient();
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Home />
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Home />
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
